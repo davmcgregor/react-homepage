4c30598355e07e0a2acbc37ca3b9cfb918509020
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-05-25 17:42:35 +0100

    Fix invite button padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,22 +4,23 @@ import { Navbar } from "react-bootstrap";
 class Footer extends Component {
   render() {
     return (
-        <Navbar
-          style={{ borderTop: "3px solid black", width: "100%", textAlign: "center"}}
-          fixed="bottom"
-          bg="light"
-        >
-          <Navbar.Brand>
-              <h5>
-                Made with 
-                <span role="img" aria-label="heart">
-                  ❤️
-                </span>
-                in London
-              </h5>
-              <h6>Copyright. 2020 Otter Limited</h6>
-          </Navbar.Brand>
-        </Navbar>
+      <Navbar
+        style={{
+          borderTop: "3px solid black",
+          width: "100%",
+          textAlign: "center",
+          display: "inline"
+        }}
+        fixed="bottom"
+        bg="light"
+      >
+        <Navbar.Brand>
+          <h5>
+            Made with<span role="img" aria-label="heart">  ❤️  </span>in London
+          </h5>
+          <h6>Copyright. 2020 Otter Limited</h6>
+        </Navbar.Brand>
+      </Navbar>
     );
   }
 }
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,22 +3,25 @@ import Button from "react-bootstrap/Button";
 
 import CenteredModal from "../Modal.js";
 
-import './HomePage.css'
+import "./HomePage.css";
 
 function HomePage() {
   const [modalShow, setModalShow] = React.useState(false);
 
   return (
-    <div>
-      <div className="home-content">
-        <h1>The new way to happiness is through Otter!</h1>
-        <h4 style={{padding: "2%"}}>Be the first to know when we launch</h4>
-        <Button className="button" variant="dark" onClick={() => setModalShow(true)}>
-          Request an invite
-        </Button>
+    <div className="home-content">
+      <h1>The new way to happiness is through Otter!</h1>
+      <h4 style={{ padding: "2%" }}>Be the first to know when we launch</h4>
+      <Button
+        style={{ padding: "1% 5% 1% 5%"}}
+        className="button"
+        variant="dark"
+        onClick={() => setModalShow(true)}
+      >
+        Request an invite
+      </Button>
 
-        <CenteredModal show={modalShow} onHide={() => setModalShow(false)} />
-      </div>
+      <CenteredModal show={modalShow} onHide={() => setModalShow(false)} />
     </div>
   );
 }
